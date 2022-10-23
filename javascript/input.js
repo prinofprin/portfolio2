9b35200e8136e466c9a401030f655f032bdffcf5
prin <prinofprin@gmail.com>
2022-10-23 13:01:00 +0700

    fitst commit

---
+++ javascript/input.js
@@ -0,0 +1,75 @@
+
+// change icon to color
+document.querySelector(".line_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/line_logo.svg';
+};
+  
+  document.querySelector(".line_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/line_logo_color.svg';
+}
+
+document.querySelector(".github_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/github_logo.svg';
+};
+  
+document.querySelector(".github_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/github_logo_color.svg';
+}
+
+document.querySelector(".linkedin_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/linkedin_logo.svg';
+};
+  
+document.querySelector(".linkedin_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/linkedin_logo_color.svg';
+}
+
+document.querySelector(".facebook_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/facebook_logo.svg';
+};
+  
+document.querySelector(".facebook_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/facebook_logo_color.svg';
+}
+
+document.querySelector(".etsy_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/etsy_logo.svg';
+};
+  
+ document.querySelector(".etsy_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/etsy_logo_color.svg';
+}
+
+document.querySelector(".email_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/email_logo.svg';
+};
+  
+  document.querySelector(".email_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/email_logo_color.svg';
+}
+
+document.querySelector(".footer-linkedin_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/linkedin_logo.svg';
+};
+  
+document.querySelector(".footer-linkedin_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/linkedin_logo_color.svg';
+}
+
+document.querySelector(".footer-line_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/line_logo.svg';
+};
+  
+  document.querySelector(".footer-line_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/line_logo_color.svg';
+}
+
+document.querySelector(".footer-email_color_hover").onmouseout = function () {
+    this.src = 'images/decorations/email_logo.svg';
+};
+  
+  document.querySelector(".footer-email_color_hover").onmouseover = function () {
+    this.src = 'images/decorations/email_logo_color.svg';
+}
+
+
